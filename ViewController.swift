19655fcfeb2e4b8e43ec735536fca89d68945b1f
Alexey Ivanov <aivanov@mobiledimension.ru>
2019-07-10 17:40:01 +0300

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WheelControl
+//
+//  Created by Алексей Иванов on 10/07/2019.
+//  Copyright © 2019 Alexey Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
